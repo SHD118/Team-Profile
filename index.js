d38df348779de99dceb9b77e3eeef7518b3beacf
Hiro Protagonist <shdesai118@gmail.com>
2021-11-03 08:39:37 -0600

    added prompt code

--- index.js
+++ index.js
@@ -1,2 +1,35 @@
 const inqurier = require("inquirer")
 const fs = require("fs")
+
+promptManager()
+
+function promptManager ()  {
+    inqurier.prompt([
+        {
+            type: "input",
+            message: "what is your name",
+            name: "name"
+        },
+        {
+            type: "input",
+            message: "what is your id",
+            name: "id"
+        },
+        {
+            type: "input",
+            message: "what is your email",
+            name: "email"
+        },
+        {
+            type: "input",
+            message: "what is your office number",
+            name: "officeNumber"
+        },
+        
+    ])
+        .then((data) => {
+            console.log("here")
+        managerArray.push(new Manager (data.name, data.id, data.email, data.officeNumber))
+        selectRole()
+    })
+}
